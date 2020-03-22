bf3cb05a702a6bb40367791735e5c92ec49ab60d
jjsebastianfuertes <jjsebastianfuertes@gmail.com>
2020-03-22 10:54:04 -0500

    Add: hero animation image-product

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -22,10 +22,13 @@ class Productitem extends StatelessWidget {
                 arguments: product.id,
               );
             },
-            child: FadeInImage(
-              placeholder: AssetImage('assets/images/image_placeholder.png'),
-              image: NetworkImage(product.imageUrl),
-              fit: BoxFit.cover,
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder: AssetImage('assets/images/image_placeholder.png'),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           footer: GridTileBar(
